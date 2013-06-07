ce1484ab112d838e2ef086923e461418b891ac06
Joe Bologna <joebologna@mac.com>
2013-06-07 10:44:10 -0500

    laid screen out to match .xib templates. no buttons yet.

--- EBD/ViewController.m
+++ EBD/ViewController.m
@@ -12,18 +12,17 @@
 
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
-#define FIELDSIZE fieldsize
-#define BUTTONSIZE buttonsize
-
 #define FTAG 100
 #define BTAG 200
+#define LTAG 300
 
 typedef enum {
     PriceA = FTAG, NumUnitsA = FTAG + 1,
     PriceB = FTAG + 2, NumUnitsB = FTAG + 3,
     Quantity = FTAG + 4,
     UnitPriceA = FTAG + 5, UnitPriceB = FTAG + 6,
-    Result = FTAG + 7
+    Result = FTAG + 7,
+    Ad = 999
 } Field;
 
 #define nInputFields (Quantity - FTAG + 1)
@@ -33,25 +32,19 @@ typedef enum {
 } DeviceType;
 
 typedef struct {
-    CGFloat text, label, button;
-} FontSizes;
-
-typedef struct {
-    CGFloat x, y, w, h;
-    char *label, *text;
+    CGRect rect;
+    CGFloat f;
 } fieldStruct;
 
 typedef struct {
-    CGFloat x, y, w, h;
+    fieldStruct field;
     char *label;
-} buttonStruct;
+} labelStruct;
 
 @interface ViewController () <ADBannerViewDelegate> {
     NSMutableArray *fieldValues;
     UIColor *fieldColor, *curFieldColor;
     DeviceType deviceType;
-    FontSizes fontSizes;
-    CGFloat fieldsize, buttonsize;
     NSInteger curField;
 }
 
@@ -64,45 +57,19 @@ typedef struct {
     [banner setHidden:NO];
 }
 
-- (void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error {
-    NSLog(@"%s", __func__);
-    [banner setHidden:YES];
-}
-
-- (BOOL)bannerViewActionShouldBegin:(ADBannerView *)banner willLeaveApplication:(BOOL)willLeave {
-    NSLog(@"%s, Nothing to do", __func__);
-    return YES;
-}
-
-- (void)bannerViewActionDidFinish:(ADBannerView *)banner {
-    NSLog(@"%s, Nothing to do", __func__);
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    ((ADBannerView *)[self.view viewWithTag:999]).delegate = self;
+    ((ADBannerView *)[self.view viewWithTag:Ad]).delegate = self;
     
     // iPhone 4 = 480, iPhone 5 = 568, iPad > 568
     CGFloat height = [UIScreen mainScreen].bounds.size.height;
     if (height <= 480) {
         deviceType = iPhone4;
-        fieldsize = 30.0;
-        buttonsize = 48.0;
-        fontSizes.text = fontSizes.label = 12;
-        fontSizes.button = 20;
     } else if (height > 480 && height <= 568) {
         deviceType = iPhone5;
-        fieldsize = 36.0;
-        buttonsize = 48.0;
-        fontSizes.text = fontSizes.label = 12;
-        fontSizes.button = 20;
     } else {
         deviceType = iPad;
-        fieldsize = 72.0;
-        buttonsize = 128.0;
-        fontSizes.text = fontSizes.label = 24;
-        fontSizes.button = 48;
     }
     
     fieldColor = UIColorFromRGB(0x86e4ae);
@@ -132,33 +99,31 @@ typedef struct {
     }
 }
 
-#define FIELD(x, y, w, t) { x, ((y * 25) + (y - 1) * FIELDSIZE), w, FIELDSIZE, t, "" }
-#define LARGEFIELD 144
-#define SMALLFIELD 104
+#define FIELD(x, y, w, h, t) { x, y, w, h, t }
 
 - (void)makeFields {
     fieldStruct fields4[] = {
-        FIELD(10, 0.8, LARGEFIELD, "Price"), FIELD(10, 1.7, SMALLFIELD, "# of Units"),
-        FIELD(164, 0.8, LARGEFIELD, "Price"), FIELD(202, 1.7, SMALLFIELD, "# of Units"),
-        FIELD(105, 2.3, SMALLFIELD, "Quantity"),
-        FIELD(10, 3.1, LARGEFIELD, "Unit Price"), FIELD(164, 3.1, LARGEFIELD, "Unit Price"),
-        FIELD(10, 4.0, LARGEFIELD*2+10, "Result")
+        FIELD(70, 29, 111, 30, 17), FIELD(189, 29, 111, 30, 17),
+        FIELD(70, 65, 111, 30, 17), FIELD(189, 65, 111, 30, 17),
+        FIELD(70, 101, 230, 30, 17),
+        FIELD(70, 137, 111, 30, 17), FIELD(189, 137, 111, 30, 17),
+        FIELD(20, 173, 280, 30, 17)
     };
     
     fieldStruct fields5[] = {
-        FIELD(10, 1, LARGEFIELD, "Price"), FIELD(10, 1.825, SMALLFIELD, "# of Units"),
-        FIELD(164, 1, LARGEFIELD, "Price"), FIELD(200, 1.825, SMALLFIELD, "# of Units"),
-        FIELD(105, 2.8, SMALLFIELD, "Quantity"),
-        FIELD(10, 3.8, LARGEFIELD, "Unit Price"), FIELD(164, 3.8, LARGEFIELD, "Unit Price"),
-        FIELD(10, 4.3, LARGEFIELD*2+10, "Result")
+        FIELD(70, 24, 111, 47, 18), FIELD(189, 24, 111, 47, 18),
+        FIELD(70, 79, 111, 47, 18), FIELD(189, 79, 111, 47, 18),
+        FIELD(70, 135, 230, 47, 18),
+        FIELD(70, 189, 111, 47, 18), FIELD(189, 189, 111, 47, 18),
+        FIELD(20, 244, 280, 47, 18)
     };
     
     fieldStruct fieldsiP[] = {
-        FIELD(20, 1.1, LARGEFIELD*2.5, "Price"), FIELD(20, 2.2, SMALLFIELD*2.5, "# of Units"), 
-        FIELD(20 + 154*2.4, 1.1, LARGEFIELD*2.5, "Price"), FIELD(20 + 8 + 190*2.4, 2.2, SMALLFIELD*2.5, "# of Units"),
-        FIELD(20 + 95*2.5 - 4, 3.0, SMALLFIELD*2.5, "Quantity"),
-        FIELD(20, 4.0, LARGEFIELD*2.5, "Unit Price"), FIELD(20 + 154*2.4, 4.0, LARGEFIELD*2.5, "Unit Price"),
-        FIELD(10, 4.3, LARGEFIELD*2, "Result")
+        FIELD(108, 34, 316, 86, 48), FIELD(432, 34, 316, 86, 48),
+        FIELD(108, 128, 316, 86, 48), FIELD(432, 128, 316, 86, 48),
+        FIELD(108, 223, 640, 86, 48),
+        FIELD(108, 316, 316, 86, 48), FIELD(432, 316, 316, 86, 48),
+        FIELD(20, 409, 728, 86, 48)
     };
     
     fieldStruct *deviceFields[] = {
@@ -166,50 +131,38 @@ typedef struct {
     };
     
     for (int i = 0; i < sizeof(fields4)/sizeof(fieldStruct); i++) {
-        CGRect rect = CGRectMake(deviceFields[deviceType][i].x,
-                                 deviceFields[deviceType][i].y,
-                                 deviceFields[deviceType][i].w,
-                                 deviceFields[deviceType][i].h);
-        [self makeButton:
-         [NSString stringWithUTF8String:deviceFields[deviceType][i].text]
-                   label:[NSString stringWithUTF8String:deviceFields[deviceType][i].label] rect:rect tag:(FTAG + i)];
+        [self makeButton:deviceFields[deviceType][i] tag:(FTAG + i) gradient:NO];
     }
     
-#define BUTTON(m, x, y, o, t)  { m + x * 4 + 10 + BUTTONSIZE * x, 64 + y * BUTTONSIZE + o + ((4 + y) * FIELDSIZE), BUTTONSIZE, BUTTONSIZE, t }
-#define LASTBUTTON(m, x, y, o, t)  { m + x * 4 + 10 + BUTTONSIZE * x, 64 + y * BUTTONSIZE + o + ((4 + y) * FIELDSIZE), BUTTONSIZE + ((deviceType == iPad) ? 136 : 52), BUTTONSIZE, t }
-#define LM 46
+#define LABEL(x, y, w, h, f, t) { x, y, w, h, f, t}
 
-    buttonStruct buttons4[] = {
-        BUTTON(LM, 0, 0, 38, "1"), BUTTON(LM, 1, 0, 38, "2"), BUTTON(LM, 2, 0, 38, "3"), BUTTON(LM, 3, 0, 38, "C"),
-        BUTTON(LM, 0, 1, 12, "4"), BUTTON(LM, 1, 1, 12, "5"), BUTTON(LM, 2, 1, 12, "6"), BUTTON(LM, 3, 1, 12, "Help"),
-        BUTTON(LM, 0, 2, -34, "7"), BUTTON(LM, 1, 2, -14, "8"), BUTTON(LM, 2, 2, -14, "9"), BUTTON(LM, 3, 2, -14, "Del"),
-        BUTTON(LM, 0, 3, -56, "."), BUTTON(LM, 1, 3, -56, "0"), LASTBUTTON(LM, 2, 3, -56, "Next")
+    labelStruct labels4[] = {
+        LABEL(8, 29, 56, 21, 12, "Price"),
+        LABEL(8, 65, 56, 21, 12, "# of Units"),
+        LABEL(8, 101, 56, 21, 12, "Quantity"),
+        LABEL(8, 137, 56, 21, 12, "Unit Cost")
     };
 
-    buttonStruct buttons5[] = {
-        BUTTON(LM, 0, 0, BUTTONSIZE, "1"), BUTTON(LM, 1, 0, BUTTONSIZE, "2"), BUTTON(LM, 2, 0, BUTTONSIZE, "3"), BUTTON(LM, 3, 0, BUTTONSIZE, "C"),
-        BUTTON(LM, 0, 1, 16, "4"), BUTTON(LM, 1, 1, 16, "5"), BUTTON(LM, 2, 1, 16, "6"), BUTTON(LM, 3, 1, 16, "Help"),
-        BUTTON(LM, 0, 2, -16, "7"), BUTTON(LM, 1, 2, -16, "8"), BUTTON(LM, 2, 2, -16, "9"), BUTTON(LM, 3, 2, -16, "Del"),
-        BUTTON(LM, 0, 3, -48, "."), BUTTON(LM, 1, 3, -48, "0"), LASTBUTTON(LM, 2, 3, -100, "Next")
+    labelStruct labels5[] = {
+        LABEL(8, 24, 56, 21, 13, "Price"),
+        LABEL(8, 79, 56, 21, 13, "# of Units"),
+        LABEL(8, 135, 56, 21, 13, "Quantity"),
+        LABEL(8, 189, 56, 21, 13, "Unit Cost")
     };
 
-    buttonStruct buttonsiP[] = {
-        BUTTON(LM*2+20, 0, 0, 48, "1"), BUTTON(LM*2+20, 1, 0, 48, "2"), BUTTON(LM*2+20, 2, 0, 48, "3"), BUTTON(LM*2+20, 3, 0, 48, "C"),
-        BUTTON(LM*2+20, 0, 1, -20, "4"), BUTTON(LM*2+20, 1, 1, -20, "5"), BUTTON(LM*2+20, 2, 1, -20, "6"), BUTTON(LM*2+20, 3, 1, -20, "Help"),
-        BUTTON(LM*2+20, 0, 2, -88, "7"), BUTTON(LM*2+20, 1, 2, -88, "8"), BUTTON(LM*2+20, 2, 2, -88, "9"), BUTTON(LM*2+20, 3, 2, -88, "Del"),
-        BUTTON(LM*2+20, 0, 3, -156, "."), BUTTON(LM*2+20, 1, 3, -156, "0"), LASTBUTTON(LM*2+20, 2, 3, -156, "Next")
+    labelStruct labelsiP[] = {
+        LABEL(8, 34, 56, 21, 13, "Price"),
+        LABEL(8, 128, 56, 21, 13, "# of Units"),
+        LABEL(8, 223, 56, 21, 13, "Quantity"),
+        LABEL(8, 316, 56, 21, 13, "Unit Cost")
     };
     
-    buttonStruct *deviceButtons[] = {
-        buttons4, buttons5, buttonsiP
+    labelStruct *deviceLabels[] = {
+        labels4, labels5, labelsiP
     };
 
-    for (int i = 0; i < sizeof(buttons4)/sizeof(buttonStruct); i++) {
-        CGRect rect = CGRectMake(deviceButtons[deviceType][i].x,
-                                 deviceButtons[deviceType][i].y,
-                                 deviceButtons[deviceType][i].w,
-                                 deviceButtons[deviceType][i].h);
-        [self makeButton:[NSString stringWithUTF8String:deviceButtons[deviceType][i].label] label:nil rect:rect tag:(BTAG + i)];
+    for (int i = 0; i < sizeof(labels4)/sizeof(labelStruct); i++) {
+        [self makeLabel:deviceLabels[deviceType][i] tag:(LTAG + i)];
     }
     
 }
@@ -353,40 +306,29 @@ typedef struct {
     }
 }
 
-- (void)makeButton:(NSString *)text label:(NSString *)label rect:(CGRect)rect tag:(NSInteger)tag  {
+- (void)makeButton:(fieldStruct)field tag:(NSInteger)tag gradient:(BOOL)gradient {
     // if a label is missing it's on the keypad, therefore it needs a gradient
-    MyButton *b = [[MyButton alloc] initWithFrame:rect];
+    MyButton *b = [[MyButton alloc] initWithFrame:field.rect];
     [b addTarget:self action:@selector(buttonPushed:) forControlEvents:UIControlEventTouchUpInside];
     [b setTitleColors:[NSArray arrayWithObjects:[UIColor blackColor], [UIColor whiteColor], nil]];
     [b setBackgroundColors:[NSArray arrayWithObjects:fieldColor, curFieldColor, [UIColor grayColor], nil]];
-    [b setTitle:text forState:UIControlStateNormal];
-    [b setTitle:text forState:UIControlStateSelected];
-    b.titleLabel.font = [UIFont systemFontOfSize:fontSizes.button];
+    b.titleLabel.font = [UIFont systemFontOfSize:field.f];
     b.tag = tag;
-    if (label == nil) {
-        if ([text isEqualToString:@"Next"]) {
-            [b setBackgroundImage:[UIImage imageNamed:(deviceType == iPad) ? @"ButtonGradient4.png" : @"ButtonGradient2.png"] forState:UIControlStateNormal];
-            [b setBackgroundImage:[UIImage imageNamed:(deviceType == iPad) ? @"ButtonGradient4.png" : @"ButtonGradient2.png"] forState:UIControlStateSelected];
-        } else {
-            [b setBackgroundImage:[UIImage imageNamed:(deviceType == iPad) ? @"ButtonGradient3.png" : @"ButtonGradient.png"] forState:UIControlStateNormal];
-            [b setBackgroundImage:[UIImage imageNamed:(deviceType == iPad) ? @"ButtonGradient3.png" : @"ButtonGradient.png"] forState:UIControlStateSelected];
-        }
-    } else {
-        [b setBackgroundColor:(tag == curField) ? fieldColor : curFieldColor];
+    if (gradient) {
+        [b setBackgroundImage:[UIImage imageNamed:@"ButtonGradient.png"] forState:UIControlStateNormal];
+        [b setBackgroundImage:[UIImage imageNamed:@"ButtonGradient.png"] forState:UIControlStateSelected];
     }
-    b.frame = rect;
+    [b setBackgroundColor:(tag == curField) ? fieldColor : curFieldColor];
     [self.view addSubview:b];
-    
-    if (label != nil) {
-        CGFloat labelSize = rect.size.height;
-        UILabel *l = [[UILabel alloc] initWithFrame:rect];
-        l.font = [UIFont systemFontOfSize:fontSizes.label * .8];
-        l.text = label;
-        l.textAlignment = NSTextAlignmentCenter;
-        l.transform = CGAffineTransformMakeTranslation(0, -labelSize * 0.8);
-        l.backgroundColor = [UIColor clearColor];
-        [self.view addSubview:l];
-    }
+}
+
+- (void)makeLabel:(labelStruct)label tag:(NSInteger)tag  {
+    UILabel *l = [[UILabel alloc] initWithFrame:label.field.rect];
+    l.font = [UIFont systemFontOfSize:label.field.f];
+    l.text = [NSString stringWithCString:label.label encoding:NSASCIIStringEncoding];
+    l.textAlignment = NSTextAlignmentLeft;
+    l.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:l];
 }
 
 - (void)showResult:(BOOL)noQty {
@@ -417,6 +359,20 @@ typedef struct {
     }
     fieldValues[Result - FTAG] = result; 
     [self updateFields];
+}
+
+- (void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error {
+    NSLog(@"%s", __func__);
+    [banner setHidden:YES];
+}
+
+- (BOOL)bannerViewActionShouldBegin:(ADBannerView *)banner willLeaveApplication:(BOOL)willLeave {
+    NSLog(@"%s, Nothing to do", __func__);
+    return YES;
+}
+
+- (void)bannerViewActionDidFinish:(ADBannerView *)banner {
+    NSLog(@"%s, Nothing to do", __func__);
 }
 
 - (void)didReceiveMemoryWarning
