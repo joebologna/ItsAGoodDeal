c600ccbfbb2de1777a2e434aa763e91f343c8521
Joe Bologna <joebologna@mac.com>
2013-06-07 12:45:57 -0500

    fixed layout for all devices

--- EBD/ViewController.m
+++ EBD/ViewController.m
@@ -15,6 +15,7 @@
 #define FTAG 100
 #define BTAG 200
 #define LTAG 300
+#define KTAG 400
 
 typedef enum {
     PriceA = FTAG, NumUnitsA = FTAG + 1,
@@ -28,16 +29,12 @@ typedef enum {
 #define nInputFields (Quantity - FTAG + 1)
 
 typedef enum {
-    iPhone4 = 0, iPhone5 = 1, iPad =2
+    iPhone4 = 0, iPhone5 = 1, iPad = 2
 } DeviceType;
 
 typedef struct {
     CGRect rect;
     CGFloat f;
-} fieldStruct;
-
-typedef struct {
-    fieldStruct field;
     char *label;
 } labelStruct;
 
@@ -76,7 +73,7 @@ typedef struct {
     curFieldColor = UIColorFromRGB(0x86ffcf);
 
     self.view.backgroundColor = [UIColor colorWithRed:0.326184 green:0.914025 blue:0.620324 alpha:1];
-    [self makeFields];
+    [self populateScreen];
     [self initGUI];
 }
 
@@ -99,62 +96,61 @@ typedef struct {
     }
 }
 
-#define FIELD(x, y, w, h, t) { x, y, w, h, t }
+#define LABEL(x, y, w, h, f, t) { x, y, w, h, f, t}
+#define YO(y) (y - 18)
 
-- (void)makeFields {
-    fieldStruct fields4[] = {
-        FIELD(70, 29, 111, 30, 17), FIELD(189, 29, 111, 30, 17),
-        FIELD(70, 65, 111, 30, 17), FIELD(189, 65, 111, 30, 17),
-        FIELD(70, 101, 230, 30, 17),
-        FIELD(70, 137, 111, 30, 17), FIELD(189, 137, 111, 30, 17),
-        FIELD(20, 173, 280, 30, 17)
+- (void)populateScreen {
+    labelStruct fields4[] = {
+        LABEL(70, YO(29), 111, 30, 17, ""), LABEL(189, YO(29), 111, 30, 17, ""),
+        LABEL(70, YO(65), 111, 30, 17, ""), LABEL(189, YO(65), 111, 30, 17, ""),
+        LABEL(70, YO(101), 230, 30, 17, ""),
+        LABEL(70, YO(137), 111, 30, 17, ""), LABEL(189, YO(137), 111, 30, 17, ""),
+        LABEL(20, YO(173), 280, 30, 17, "")
     };
     
-    fieldStruct fields5[] = {
-        FIELD(70, 24, 111, 47, 18), FIELD(189, 24, 111, 47, 18),
-        FIELD(70, 79, 111, 47, 18), FIELD(189, 79, 111, 47, 18),
-        FIELD(70, 135, 230, 47, 18),
-        FIELD(70, 189, 111, 47, 18), FIELD(189, 189, 111, 47, 18),
-        FIELD(20, 244, 280, 47, 18)
+    labelStruct fields5[] = {
+        LABEL(70, YO(24), 111, 47, 18, ""), LABEL(189, YO(24), 111, 47, 18, ""),
+        LABEL(70, YO(79), 111, 47, 18, ""), LABEL(189, YO(79), 111, 47, 18, ""),
+        LABEL(70, YO(135), 230, 47, 18, ""),
+        LABEL(70, YO(189), 111, 47, 18, ""), LABEL(189, YO(189), 111, 47, 18, ""),
+        LABEL(20, YO(244), 280, 47, 18, "")
     };
     
-    fieldStruct fieldsiP[] = {
-        FIELD(108, 34, 316, 86, 48), FIELD(432, 34, 316, 86, 48),
-        FIELD(108, 128, 316, 86, 48), FIELD(432, 128, 316, 86, 48),
-        FIELD(108, 223, 640, 86, 48),
-        FIELD(108, 316, 316, 86, 48), FIELD(432, 316, 316, 86, 48),
-        FIELD(20, 409, 728, 86, 48)
+    labelStruct fieldsiP[] = {
+        LABEL(108, YO(34), 316, 86, 48, ""), LABEL(432, YO(34), 316, 86, 48, ""),
+        LABEL(108, YO(128), 316, 86, 48, ""), LABEL(432, YO(128), 316, 86, 48, ""),
+        LABEL(108, YO(223), 640, 86, 48, ""),
+        LABEL(108, YO(316), 316, 86, 48, ""), LABEL(432, YO(316), 316, 86, 48, ""),
+        LABEL(24, YO(409), 724, 86, 48, "")
     };
     
-    fieldStruct *deviceFields[] = {
+    labelStruct *deviceFields[] = {
         fields4, fields5, fieldsiP
     };
     
-    for (int i = 0; i < sizeof(fields4)/sizeof(fieldStruct); i++) {
-        [self makeButton:deviceFields[deviceType][i] tag:(FTAG + i) gradient:NO];
+    for (int i = 0; i < sizeof(fields4)/sizeof(labelStruct); i++) {
+        [self makeButton:deviceFields[deviceType][i] tag:(FTAG + i) isKey:NO];
     }
     
-#define LABEL(x, y, w, h, f, t) { x, y, w, h, f, t}
-
     labelStruct labels4[] = {
-        LABEL(8, 29, 56, 21, 12, "Price"),
-        LABEL(8, 65, 56, 21, 12, "# of Units"),
-        LABEL(8, 101, 56, 21, 12, "Quantity"),
-        LABEL(8, 137, 56, 21, 12, "Unit Cost")
+        LABEL(8, YO(29), 56, 21, 12, "Price"),
+        LABEL(8, YO(65), 56, 21, 12, "# of Units"),
+        LABEL(8, YO(101), 56, 21, 12, "Quantity"),
+        LABEL(8, YO(137), 56, 21, 12, "Unit Cost")
     };
 
     labelStruct labels5[] = {
-        LABEL(8, 24, 56, 21, 13, "Price"),
-        LABEL(8, 79, 56, 21, 13, "# of Units"),
-        LABEL(8, 135, 56, 21, 13, "Quantity"),
-        LABEL(8, 189, 56, 21, 13, "Unit Cost")
+        LABEL(8, YO(24), 56, 21, 13, "Price"),
+        LABEL(8, YO(79), 56, 21, 13, "# of Units"),
+        LABEL(8, YO(135), 56, 21, 13, "Quantity"),
+        LABEL(8, YO(189), 56, 21, 13, "Unit Cost")
     };
 
     labelStruct labelsiP[] = {
-        LABEL(8, 34, 56, 21, 13, "Price"),
-        LABEL(8, 128, 56, 21, 13, "# of Units"),
-        LABEL(8, 223, 56, 21, 13, "Quantity"),
-        LABEL(8, 316, 56, 21, 13, "Unit Cost")
+        LABEL(8, YO(34), 80, 21, 18, "Price"),
+        LABEL(8, YO(128), 80, 21, 18, "# of Units"),
+        LABEL(8, YO(223), 80, 21, 18, "Quantity"),
+        LABEL(8, YO(316), 80, 21, 18, "Unit Cost")
     };
     
     labelStruct *deviceLabels[] = {
@@ -165,6 +161,69 @@ typedef struct {
         [self makeLabel:deviceLabels[deviceType][i] tag:(LTAG + i)];
     }
     
+#define LABEL(x, y, w, h, f, t) { x, y, w, h, f, t}
+    
+    labelStruct keypad4[] = {
+        LABEL(20, YO(211), 64, 48, 15, "1"),
+        LABEL(92, YO(211), 64, 48, 15, "2"),
+        LABEL(164, YO(211), 64, 48, 15, "3"),
+        LABEL(236, YO(211), 64, 48, 15, "C"),
+        LABEL(20, YO(266), 64, 48, 15, "4"),
+        LABEL(92, YO(266), 64, 48, 15, "5"),
+        LABEL(164, YO(266), 64, 48, 15, "6"),
+        LABEL(236, YO(266), 64, 48, 15, "Help"),
+        LABEL(20, YO(320), 64, 48, 15, "7"),
+        LABEL(92, YO(320), 64, 48, 15, "8"),
+        LABEL(164, YO(320), 64, 48, 15, "9"),
+        LABEL(236, YO(320), 64, 48, 15, "Del"),
+        LABEL(20, YO(375), 64, 48, 15, "."),
+        LABEL(92, YO(375), 64, 48, 15, "0"),
+        LABEL(164, YO(375), 136, 48, 15, "Next")
+    };
+    
+    labelStruct keypad5[] = {
+        LABEL(20, YO(299), 64, 48, 15, "1"),
+        LABEL(92, YO(299), 64, 48, 15, "2"),
+        LABEL(164, YO(299), 64, 48, 15, "3"),
+        LABEL(236, YO(299), 64, 48, 15, "C"),
+        LABEL(20, YO(354), 64, 48, 15, "4"),
+        LABEL(92, YO(354), 64, 48, 15, "5"),
+        LABEL(164, YO(354), 64, 48, 15, "6"),
+        LABEL(236, YO(354), 64, 48, 15, "Help"),
+        LABEL(20, YO(408), 64, 48, 15, "7"),
+        LABEL(92, YO(408), 64, 48, 15, "8"),
+        LABEL(164, YO(408), 64, 48, 15, "9"),
+        LABEL(236, YO(408), 64, 48, 15, "Del"),
+        LABEL(20, YO(463), 64, 48, 15, "."),
+        LABEL(92, YO(463), 64, 48, 15, "0"),
+        LABEL(164, YO(463), 136, 48, 15, "Next")
+    };
+    
+    labelStruct keypadiP[] = {
+        LABEL(24, YO(510), 174, 105, 48, "1"),
+        LABEL(206, YO(510), 174, 105, 48, "2"),
+        LABEL(388, YO(510), 174, 105, 48, "3"),
+        LABEL(570, YO(510), 174, 105, 48, "C"),
+        LABEL(24, YO(623), 174, 105, 48, "4"),
+        LABEL(206, YO(623), 174, 105, 48, "5"),
+        LABEL(388, YO(623), 174, 105, 48, "6"),
+        LABEL(570, YO(623), 174, 105, 48, "Help"),
+        LABEL(24, YO(734), 174, 105, 48, "7"),
+        LABEL(206, YO(734), 174, 105, 48, "8"),
+        LABEL(388, YO(734), 174, 105, 48, "9"),
+        LABEL(570, YO(734), 174, 105, 48, "Del"),
+        LABEL(24, YO(845), 174, 105, 48, "."),
+        LABEL(206, YO(845), 174, 105, 48, "0"),
+        LABEL(388, YO(845), 356, 105, 48, "Next")
+    };
+    
+    labelStruct *deviceKeys[] = {
+        keypad4, keypad5, keypadiP
+    };
+    
+    for (int i = 0; i < sizeof(keypad4)/sizeof(labelStruct); i++) {
+        [self makeButton:deviceKeys[deviceType][i] tag:(KTAG + i) isKey:YES];
+    }
 }
 
 - (void)buttonPushed:(MyButton *)sender {
@@ -181,63 +240,63 @@ typedef struct {
     } else if (sender.tag >= BTAG) {
         NSString *key = @"";
         switch (sender.tag) {
-            case BTAG:
+            case KTAG:
                 key = @"1";
                 break;
                 
-            case BTAG + 1:
+            case KTAG + 1:
                 key = @"2";
                 break;
                 
-            case BTAG + 2:
+            case KTAG + 2:
                 key = @"3";
                 break;
                 
-            case BTAG + 3:
+            case KTAG + 3:
                 key = @"C";
                 break;
                 
-            case BTAG + 4:
+            case KTAG + 4:
                 key = @"4";
                 break;
                 
-            case BTAG + 5:
+            case KTAG + 5:
                 key = @"5";
                 break;
                 
-            case BTAG + 6:
+            case KTAG + 6:
                 key = @"6";
                 break;
                 
-            case BTAG + 7:
+            case KTAG + 7:
                 key = @"Help";
                 break;
                 
-            case BTAG + 8:
+            case KTAG + 8:
                 key = @"7";
                 break;
                 
-            case BTAG + 9:
+            case KTAG + 9:
                 key = @"8";
                 break;
                 
-            case BTAG + 10:
+            case KTAG + 10:
                 key = @"9";
                 break;
                 
-            case BTAG + 11:
+            case KTAG + 11:
                 key = @"Del";
                 break;
                 
-            case BTAG + 12:
+            case KTAG + 12:
                 key = @".";
                 break;
                 
-            case BTAG + 13:
+            case KTAG + 13:
                 key = @"0";
                 break;
                 
-            case BTAG + 14:
+            case KTAG + 14:
                 key = @"Next";
                 break;
                 
@@ -289,6 +348,12 @@ typedef struct {
             }
             [self updateFields];
         } else if ([key isEqualToString:@"Help"]) {
+            ADBannerView *a = (ADBannerView *)[self.view viewWithTag:Ad];
+            if (a.isHidden) {
+                [a setHidden:NO];
+            } else {
+                [a setHidden:YES];
+            }
         } else if ([key isEqualToString:@"."]) {
             NSRange r = [s rangeOfString:@"."];
             if (r.location == NSNotFound) {
@@ -306,25 +371,27 @@ typedef struct {
     }
 }
 
-- (void)makeButton:(fieldStruct)field tag:(NSInteger)tag gradient:(BOOL)gradient {
+- (void)makeButton:(labelStruct)label tag:(NSInteger)tag isKey:(BOOL)isKey {
     // if a label is missing it's on the keypad, therefore it needs a gradient
-    MyButton *b = [[MyButton alloc] initWithFrame:field.rect];
+    MyButton *b = [[MyButton alloc] initWithFrame:label.rect];
     [b addTarget:self action:@selector(buttonPushed:) forControlEvents:UIControlEventTouchUpInside];
     [b setTitleColors:[NSArray arrayWithObjects:[UIColor blackColor], [UIColor whiteColor], nil]];
     [b setBackgroundColors:[NSArray arrayWithObjects:fieldColor, curFieldColor, [UIColor grayColor], nil]];
-    b.titleLabel.font = [UIFont systemFontOfSize:field.f];
+    b.titleLabel.font = [UIFont systemFontOfSize:label.f];
     b.tag = tag;
-    if (gradient) {
-        [b setBackgroundImage:[UIImage imageNamed:@"ButtonGradient.png"] forState:UIControlStateNormal];
-        [b setBackgroundImage:[UIImage imageNamed:@"ButtonGradient.png"] forState:UIControlStateSelected];
+    if (isKey) {
+        [b setBackgroundImage:[UIImage imageNamed:@"ButtonGradient3.png"] forState:UIControlStateNormal];
+        [b setBackgroundImage:[UIImage imageNamed:@"ButtonGradient.3png"] forState:UIControlStateSelected];
+        [b setTitle:[NSString stringWithCString:label.label encoding:NSASCIIStringEncoding] forState:UIControlStateNormal];
+        [b setTitle:[NSString stringWithCString:label.label encoding:NSASCIIStringEncoding] forState:UIControlStateSelected];
     }
     [b setBackgroundColor:(tag == curField) ? fieldColor : curFieldColor];
     [self.view addSubview:b];
 }
 
 - (void)makeLabel:(labelStruct)label tag:(NSInteger)tag  {
-    UILabel *l = [[UILabel alloc] initWithFrame:label.field.rect];
-    l.font = [UIFont systemFontOfSize:label.field.f];
+    UILabel *l = [[UILabel alloc] initWithFrame:label.rect];
+    l.font = [UIFont systemFontOfSize:label.f];
     l.text = [NSString stringWithCString:label.label encoding:NSASCIIStringEncoding];
     l.textAlignment = NSTextAlignmentLeft;
     l.backgroundColor = [UIColor clearColor];
