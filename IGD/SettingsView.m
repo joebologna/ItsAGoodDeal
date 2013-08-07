399f276aa4a7ef5a2556d6ab144e80bc767a28cd
Joe Bologna <joebologna@mac.com>
2013-08-06 20:59:34 -0500

    updated instructions

--- IGD/SettingsView.m
+++ IGD/SettingsView.m
@@ -77,21 +77,13 @@
 
     UITextView *h = [[UITextView alloc] initWithFrame:CGRectMake(bheight, toffset + SLOT(position++), width - 2 * bheight, theight)];
     h.font = [UIFont systemFontOfSize:tfontSize];
-    h.text = @"Enter the price and number of units for each item, for instance 8 ounces. Enter 1 for the # of items.\n\nTo enter a 2 for 1 sale, enter 2 for the # of items.\n\nUse the slider to increase or decrease the quantity you plan to purchase. The price for each item and savings is adjusted automatically. The slider is limited to 10 items by default. Enter a number greater than 10 in the Qty box to increase the slider limit.\n\nYou may purchase the Remove Ads option or restore your purchase on other devices you own.";
+    h.text = @"Enter the price and number of units for each item, for instance 8 ounces. Enter 1 for the # of items.\n\nTo enter a 2 for 1 sale, enter 2 for the # of items.\n\nUse the slider to increase or decrease the quantity you plan to purchase. The price for each item and savings is adjusted automatically. The slider is limited to 10 items by default. Enter a number greater than 10 in the Qty box to increase the slider limit.\n\nSavings are the total number of units purchased times the difference in unit cost.\n\nYou may purchase the Remove Ads option or restore your purchase on other devices you own.";
     h.editable = NO;
     [h.layer setBorderColor:[[[UIColor blackColor] colorWithAlphaComponent:0.5] CGColor]];
     [h.layer setBorderWidth:2.0];
     [self.view addSubview:h];
     
     position = 7;
-    MyButton *r = [[MyButton alloc] initWithFrame:CGRectMake(bheight, SLOT(position++), bwidth, bheight)];
-    r.bothTitles = @"Restore";
-    r.font = [UIFont systemFontOfSize:fontSize];
-    r.radius = radius;
-    [r setTitleColors:[NSArray arrayWithObjects:[UIColor blackColor], [UIColor blackColor], nil]];
-    [r addTarget:self action:@selector(restore:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:r];
-
     MyButton *ra = [[MyButton alloc] initWithFrame:CGRectMake(bheight, SLOT(position++), bwidth, bheight)];
     ra.bothTitles = @"Remove Ads";
     ra.font = [UIFont systemFontOfSize:fontSize];
@@ -99,6 +91,14 @@
     [ra setTitleColors:[NSArray arrayWithObjects:[UIColor blackColor], [UIColor blackColor], nil]];
     [ra addTarget:self action:@selector(removeAds:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:ra];
+    
+    MyButton *r = [[MyButton alloc] initWithFrame:CGRectMake(bheight, SLOT(position++), bwidth, bheight)];
+    r.bothTitles = @"Restore";
+    r.font = [UIFont systemFontOfSize:fontSize];
+    r.radius = radius;
+    [r setTitleColors:[NSArray arrayWithObjects:[UIColor blackColor], [UIColor blackColor], nil]];
+    [r addTarget:self action:@selector(restore:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:r];
 
     MyButton *b = [[MyButton alloc] initWithFrame:CGRectMake(bheight, SLOT(position++), bwidth, bheight)];
     b.bothTitles = @"Done";
